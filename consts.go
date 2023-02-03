30ce3dca841c0d1336a8f49b0cce740ec36954d3
Arran Ubels <arran4@gmail.com>
2023-02-03 15:34:43 +1100

    Consts and comments.

---
+++ consts.go
@@ -0,0 +1,7 @@
+package ecg
+
+import "github.com/alecthomas/units"
+
+const (
+	ReadSize = 256 * units.Kibibyte
+)
--- interface.go
+++ interface.go
@@ -1,20 +1,46 @@
-package editorconfig_guesser
+package ecg
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+)
 
+// Contraster Provides contrasts two SummaryResults from the same file format for the purpose of consolidation -- future -- maybe
+type Contraster func(s1, s2 *SummaryResult) int
+
+// SummaryResult results from a file format
 type SummaryResult struct {
-	FileGlobs  []string
+	// Impacted globs, used for reconstructing.
+	FileGlobs []string
+	// How confident we are not used atm
 	Confidence float64
-	Template   fmt.Stringer
-	Path       string
+	// Future use to compare how different two file paths (same file format) are
+	Contaster Contraster
+	// The template
+	Template fmt.Stringer
+	// The path, this is for future versions where it will suggest sub-directory variants based on confidence and contrast -- maybe
+	Path string
+	// Internal data, probably going to be used by Contraster
+	Data any
 }
 
+// File reference, could also be a cache
 type File struct {
 	Filename string
 }
 
-type Language interface {
+// Open abstracter eventually might cache, perhaps checking file size first - or only caching the first 256kb
+func (f File) Open() (io.ReadCloser, error) {
+	return os.Open(f.Filename)
+}
+
+// FileFormat a file format
+type FileFormat interface {
+	// Name The display name for errors etc
 	Name() string
+	// Start starts reading files sent to it on the channel, will close on receiving a nil
 	Start() chan *File
+	// Done waits until Start() is complete, then returns the SummaryResults and/or an error
 	Done() ([]*SummaryResult, error)
 }
